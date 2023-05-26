9b435bb16620864a1167284d6f6e0716bf497451
DianaM411 <dianamunteanu220@gmail.com>
2023-05-26 22:33:40 +0200

    Likes - pero no funciona

--- backend/controllers/imagesController.js
+++ backend/controllers/imagesController.js
@@ -1,67 +1,96 @@
-import express from 'express';
-import Image from '../mongodb/models/image.js';
-import * as dotenv from 'dotenv';
-import { v2 as cloudinary } from 'cloudinary'; //Se está utilizando la versión 2 de la API, por lo que se indica con el alias v2.
+import express from "express";
+import Image from "../mongodb/models/image.js";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary"; //Se está utilizando la versión 2 de la API, por lo que se indica con el alias v2.
 
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-    secure: true,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 //Function to GET all image/post
 const getImages = async (req, res) => {
-    try {
-      const images = await Image.find({}).populate('user_id');
-      res.status(200).json({ success: true, data: images });
-    } catch (err) {
-      res.status(500).json({ success: false, message: 'Fallo al cargar las imágenes.' });
-    }
-}
+  try {
+    const images = await Image.find({}).populate("user_id");
+    res.status(200).json({ success: true, data: images });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Fallo al cargar las imágenes." });
+  }
+};
 
 //Function to POST a new image/post
 const createNewPost = async (req, res) => {
-    try {
-        const { prompt, photo, user_id } = req.body;
-        const photoUrl = (await cloudinary.uploader.upload(photo, {'folder': 'ImagiNationAI'}));
-        //const photoUrl = (await cloudinary.uploader.upload("../../frontend/src/assets/image1.png")).secure_url;
-        console.log("LLEGA AQUÍ");
-        const newImage = await Image.create({
-            prompt,
-            url: photoUrl.secure_url,
-            user_id
-        });
-        //res.status(200).json({ success: true, data: "Éxito" });
-        res.status(200).json({ success: true, data: newImage });
-    } catch (err) {
-        console.log("Lanza Error");
-        res.status(500).json({ success: false, message: 'No fue posible crear un nuevo post. Inténtalo de nuevo.' });
-    }
-}
+  try {
+    const { prompt, photo, user_id } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo, {
+      folder: "ImagiNationAI",
+    });
+    //const photoUrl = (await cloudinary.uploader.upload("../../frontend/src/assets/image1.png")).secure_url;
+    console.log("LLEGA AQUÍ");
+    const newImage = await Image.create({
+      prompt,
+      url: photoUrl.secure_url,
+      user_id,
+    });
+    //res.status(200).json({ success: true, data: "Éxito" });
+    res.status(200).json({ success: true, data: newImage });
+  } catch (err) {
+    console.log("Lanza Error");
+    res.status(500).json({
+      success: false,
+      message: "No fue posible crear un nuevo post. Inténtalo de nuevo.",
+    });
+  }
+};
 
 //Function to GET a specific image/post
 const getImage = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const image = await Image.findById(id);
-        res.status(200).json({ success: true, data: image });
-    } catch (err) {
-        res.status(500).json({ success: false, message: 'Fallo al recuperar la imagen' });
-    }
-}
+  try {
+    const { id } = req.params;
+    const image = await Image.findById(id);
+    res.status(200).json({ success: true, data: image });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Fallo al recuperar la imagen" });
+  }
+};
 
 //Function to DELETE a specific image/post
 const deletePost = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const image = await Image.findByIdAndDelete(id);
-        res.status(200).json({ success: true, data: image });
-    } catch (err) {
-        res.status(404).json({ success: false, message: 'Fallo al borrar.' });
-    }
-}
+  try {
+    const { id } = req.params;
+    const image = await Image.findByIdAndDelete(id);
+    res.status(200).json({ success: true, data: image });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "Fallo al borrar." });
+  }
+};
 
-export {getImages, getImage, createNewPost, deletePost};
+// Function to add LIKES
+const likeImage = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Find the image by ID and increment the likes property
+    const image = await Image.findByIdAndUpdate(
+      id,
+      { $inc: { likes: 1 } },
+      { new: true }
+    );
+
+    res.status(200).json({ success: true, data: image });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to like the image." });
+  }
+};
+
+export { getImages, getImage, createNewPost, deletePost, likeImage };
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,47 +1,72 @@
 import React, { useState } from "react";
+import axios from "axios";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { HandThumbUpIcon } from "@heroicons/react/24/outline";
 import { downloadIcon } from "../assets";
 import { downloadImage } from "../utils";
 
-const Card = ({ _id, user_id, prompt, url, likes }) => (
-  <div className="rounded-xl group relative hover:shadow-cardhover card">
-    <img
-      className="w-full h-full object-cover rounded-xl"
-      src={url}
-      alt={prompt}
-    />
-    <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
-      <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
+const Card = ({ _id, user_id, prompt, url, likes }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [localLikes, setLocalLikes] = useState(likes);
 
-      <div className="mt-5 flex justify-between items-center gap-2">
-        <div className="flex items-center gap-2">
-          <div className="w-7 h-7 rounded-full object-cover bg-neon-pink flex justify-center items-center text-white text-xs font-bold">
-            {user_id.name[0]}
+  const handleLike = async () => {
+    if (!isLiked) {
+      try {
+        // Send a POST request to the backend to increment the likes
+        await axios.post(`/images/${_id}/like`);
+
+        // Update the likes locally
+        setIsLiked(true);
+      } catch (error) {
+        console.log("Failed to like the image:", error);
+      }
+    }
+  };
+
+  return (
+    <div className="rounded-xl group relative hover:shadow-cardhover card">
+      <img
+        className="w-full h-full object-cover rounded-xl"
+        src={url}
+        alt={prompt}
+      />
+      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+        <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
+
+        <div className="mt-5 flex justify-between items-center gap-2">
+          <div className="flex items-center gap-2">
+            <div className="w-7 h-7 rounded-full object-cover bg-neon-pink flex justify-center items-center text-white text-xs font-bold">
+              {user_id.name[0]}
+            </div>
+            <p className="text-white text-sm">{user_id.name}</p>
           </div>
-          <p className="text-white text-sm">{user_id.name}</p>
+          <button
+            className="bg-light-grey font-semibold hover:bg-neon-pink py-2 px-4 rounded-full mx-2"
+            onClick={handleLike}
+            disabled={isLiked}
+          >
+            <HandThumbUpIcon className="h-5 w-5 text-dark-blue inline-flex" />
+            Like
+          </button>
+          <p className="text-white font-semibold mr-4">
+            <HeartIcon className="h-5 w-5 text-white inline-flex" />{" "}
+            {localLikes}{" "}
+          </p>
+          <button
+            type="button"
+            onClick={() => downloadImage(_id, url)}
+            className="outline-none bg-transparent border-none"
+          >
+            <img
+              src={downloadIcon}
+              alt="download"
+              className="w-6 h-6 object-contain invert"
+            />
+          </button>
         </div>
-        <button className="bg-light-grey font-semibold hover:bg-neon-pink py-2 px-4 rounded-full mr-4">
-          <HandThumbUpIcon className="h-5 w-5 text-dark-blue inline-flex" />
-          Like
-        </button>
-        <p className="text-white font-semibold mr-4">
-          <HeartIcon className="h-5 w-5 text-white inline-flex" /> {likes}{" "}
-        </p>
-        <button
-          type="button"
-          onClick={() => downloadImage(_id, url)}
-          className="outline-none bg-transparent border-none"
-        >
-          <img
-            src={downloadIcon}
-            alt="download"
-            className="w-6 h-6 object-contain invert"
-          />
-        </button>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
