1638df8346b6a7215c5f9dbf6b563f01f7e3830d
lmontejo91 <l.montejov.91@gmail.com>
2023-05-28 22:28:52 +0200

    Validation&MyLikes_BACK

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,8 +1,7 @@
 import Card from "./Card";
 import FormLogin from "./FormLogin";
-import FormRegister from "./FormRegister";
 import Loader from "./Loader";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export { Card, FormLogin, FormRegister, Loader, Navbar, Footer };
+export { Card, FormLogin, Loader, Navbar, Footer };
