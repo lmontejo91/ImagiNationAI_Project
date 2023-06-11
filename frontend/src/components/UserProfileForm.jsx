87fd1966ba4ba2052a35f952050f8e3dd14fc814
DianaM411 <dianamunteanu220@gmail.com>
2023-06-11 22:05:29 +0200

    Botones activo/inactivo User Page

--- frontend/src/components/UserProfileForm.jsx
+++ frontend/src/components/UserProfileForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { HiPencil, HiThumbUp } from "react-icons/hi";
 import { HiPencilSquare } from "react-icons/hi2";
 import { AuthContext } from "../utils/auth";
-import { ModalAlert, ModalConfirmation, ModalSuccess } from "./modals";
+import { ModalAlert, ModalConfirmation } from "./modals";
 import { API_URL } from "../../config";
 import { redirect } from "react-router-dom";
 
@@ -18,15 +18,16 @@ const UserProfileForm = () => {
   const [typeModal, setTypeModal] = useState("");
   const [redirect, setRedirect] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [inputBackgroundColor, setInputBackgroundColor] = useState("bg-dark-blue");  
+  const [inputBackgroundColor, setInputBackgroundColor] =
+    useState("bg-dark-blue");
   const [inputTextColor, setInputTextColor] = useState("text-muted");
   const [formUser, setFormUser] = useState({
     firstname: user.firstname,
     lastname: user.lastname,
     username: user.username,
     email: user.email,
-    password:"********",
-  });  
+    password: "********",
+  });
 
   const handleEditClick = (isEditOn) => {
     setIsEditing(isEditOn);
@@ -41,173 +42,196 @@ const UserProfileForm = () => {
   };
 
   const handleInputChange = (e) => {
-    const { name, value} = e.target;
+    const { name, value } = e.target;
     setFormUser({ ...formUser, [name]: value });
   };
 
   const handleSubmitChanges = async () => {
     try {
-      const response = await fetch(`${API_URL}/v1/user/${authContext.user._id}`, 
+      const response = await fetch(
+        `${API_URL}/v1/user/${authContext.user._id}`,
         {
           method: "PUT",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(formUser),
-        });
-        
+        }
+      );
+
       const data = await response.json();
       authContext.setUser(data.user);
-      setTitleModal(data.success ? 'Account updated successfully!' : 'Account could not be updated');
-      setTypeModal(data.success ? 'success' : 'error');
-      setMessageModal(data.message); 
+      setTitleModal(
+        data.success
+          ? "Account updated successfully!"
+          : "Account could not be updated"
+      );
+      setTypeModal(data.success ? "success" : "error");
+      setMessageModal(data.message);
       openModalAlert();
       handleEditClick(false);
-
     } catch (err) {
-      alert("Connection to the server failed: "+err);
+      alert("Connection to the server failed: " + err);
       return false;
-    }  
+    }
   };
 
   const handleDeleteAccount = async (e) => {
-      if(confirmed){
-        try {
-          const response = await fetch(`${API_URL}/v1/user/${authContext.user._id}`,
-            {
-              method: "DELETE",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-          const data = await response.json();
-          setTitleModal(data.success ? 'Account deleted successfully!' : 'Account could not be deleted');
-          setTypeModal(data.success ? 'success' : 'error');
-          setRedirect(data.success ? true : false);
-          setMessageModal(data.message);
-          openModalAlert();
-
-        } catch (err) {
-          alert("Connection to the server failed: "+err);
-        }
+    if (confirmed) {
+      try {
+        const response = await fetch(
+          `${API_URL}/v1/user/${authContext.user._id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        setTitleModal(
+          data.success
+            ? "Account deleted successfully!"
+            : "Account could not be deleted"
+        );
+        setTypeModal(data.success ? "success" : "error");
+        setRedirect(data.success ? true : false);
+        setMessageModal(data.message);
+        openModalAlert();
+      } catch (err) {
+        alert("Connection to the server failed: " + err);
       }
+    }
   };
-
 
   /* Modal Alert */
   const openModalAlert = () => {
     setIsModalAlertOpen(true);
   };
-  
+
   const closeModalAlert = () => {
     setIsModalAlertOpen(false);
-    if(redirect) 
-      authContext.logout();
+    if (redirect) authContext.logout();
   };
 
   /* Modal Confirmation */
   const openModalConfirmation = () => {
     setIsModalConfirmationOpen(true);
   };
-  
+
   const closeModalConfirmation = (isConfirmed) => {
     setConfirmed(isConfirmed);
-    if(confirmed) 
-      handleDeleteAccount();
+    if (confirmed) handleDeleteAccount();
     setIsModalConfirmationOpen(false);
   };
 
   return (
     <div className="mt-10">
       <form className="mt-7">
-        <div className="shadow-md rounded p-12 relative"> {/* ring-medium-grey ring-offset-2 ring-4 */} 
-        {isEditing ? (
-          <>
-          <button  onClick = {() => handleEditClick(false) } className="ml-2 absolute top-2 right-3">
-              <HiThumbUp className="h-6 w-6 mr-1 text-light-grey inline-flex" />{" "}
-          </button>
-          </>
-        ) : (
-          <button onClick = {() =>  handleEditClick(true) } className="ml-2 absolute top-2 right-3">
+        <div className="shadow-md rounded p-12 relative">
+          {" "}
+          {/* ring-medium-grey ring-offset-2 ring-4 */}
+          {isEditing ? (
+            <>
+              <button
+                onClick={() => handleEditClick(false)}
+                className="ml-2 absolute top-2 right-3"
+              >
+                <HiThumbUp className="h-6 w-6 mr-1 text-light-grey inline-flex" />{" "}
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => handleEditClick(true)}
+              className="ml-2 absolute top-2 right-3"
+            >
               <HiPencilSquare className="h-6 w-6 mr-1 text-light-grey inline-flex" />{" "}
-          </button>
-        )}
+            </button>
+          )}
+          {/* Form Fields */}
+          <div className="lg:col-span-2 text-light-grey">
+            <div className="grid gap-4 gap-y-6 text-sm grid-cols-1 md:grid-cols-6">
+              <div className="md:col-span-3">
+                <label htmlFor="firstname" className="font-semibold">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="firstname"
+                  id="firstname"
+                  className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`}
+                  value={formUser.firstname}
+                  placeholder={formUser.firstname}
+                  readOnly
+                  onChange={handleInputChange}
+                />
+              </div>
 
-        {/* Form Fields */}
-        <div className="lg:col-span-2 text-light-grey">
-          <div className="grid gap-4 gap-y-6 text-sm grid-cols-1 md:grid-cols-6">
+              <div className="md:col-span-3">
+                <label htmlFor="lastname" className="font-semibold">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lastname"
+                  id="lastname"
+                  className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`}
+                  value={formUser.lastname}
+                  placeholder={formUser.lastname}
+                  readOnly
+                  onChange={handleInputChange}
+                />
+              </div>
 
-          <div className="md:col-span-3">
-              <label htmlFor="firstname" className="font-semibold">First Name</label>
-              <input 
-                type="text" 
-                name="firstname" 
-                id="firstname" 
-                className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`} 
-                value={formUser.firstname} 
-                placeholder={formUser.firstname} 
-                readOnly
-                onChange={handleInputChange}
-              />
-            </div>
+              <div className="md:col-span-6">
+                <label htmlFor="username" className="font-semibold">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`}
+                  value={formUser.username}
+                  placeholder={formUser.username}
+                  readOnly
+                  onChange={handleInputChange}
+                />
+              </div>
 
-            <div className="md:col-span-3">
-              <label htmlFor="lastname" className="font-semibold">Last Name</label>
-              <input 
-                type="text" 
-                name="lastname" 
-                id="lastname" 
-                className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`} 
-                value={formUser.lastname}  
-                placeholder={formUser.lastname}
-                readOnly
-                onChange={handleInputChange} 
-              />
-            </div>
+              <div className="md:col-span-6">
+                <label htmlFor="email" className="font-semibold">
+                  Email Address
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`}
+                  value={formUser.email}
+                  placeholder={formUser.email}
+                  readOnly
+                  onChange={handleInputChange}
+                />
+              </div>
 
-            <div className="md:col-span-6">
-              <label htmlFor="username" className="font-semibold">Username</label>
-              <input 
-                type="text" 
-                name="username" 
-                id="username" 
-                className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`} 
-                value={formUser.username}  
-                placeholder={formUser.username} 
-                readOnly
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="md:col-span-6">
-              <label htmlFor="email" className="font-semibold">Email Address</label>
-              <input 
-                type="text" 
-                name="email" 
-                id="email" 
-                className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`} 
-                value={formUser.email} 
-                placeholder={formUser.email}
-                readOnly
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div className="md:col-span-6">
-              <label htmlFor="password" className="font-semibold">Password</label>
-              <input 
-                type="password" 
-                name="password" 
-                id="password" 
-                className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`} 
-                value={formUser.password}
-                placeholder="Type here your old password"
-                readOnly
-                onChange={handleInputChange}
-              />
+              <div className="md:col-span-6">
+                <label htmlFor="password" className="font-semibold">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className={`h-10 border-b mt-1 rounded px-4 w-full ${inputBackgroundColor} ${inputTextColor} focus:outline-none focus:ring-0`}
+                  value={formUser.password}
+                  placeholder="Type here your old password"
+                  readOnly
+                  onChange={handleInputChange}
+                />
+              </div>
             </div>
           </div>
-        </div>
         </div>
         {/* Form Buttons */}
         <div className="flex justify-center mt-12 mb-6">
--- frontend/src/components/modals/index.js
+++ frontend/src/components/modals/index.js
@@ -1,5 +1,4 @@
 import ModalAlert from "./ModalAlert";
 import ModalConfirmation from "./ModalConfirmation";
-import ModalSuccess from "./ModalSuccess";
 
-export { ModalAlert, ModalConfirmation, ModalSuccess };
+export { ModalAlert, ModalConfirmation };
