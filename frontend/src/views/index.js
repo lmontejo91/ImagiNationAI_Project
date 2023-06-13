38c66a6c1d5f639824aaa99d3777917cc5ee23c8
lmontejo91 <l.montejov.91@gmail.com>
2023-06-13 22:38:31 +0200

    littleChanges

--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -2,6 +2,7 @@ import HomePage from "./HomePage";
 import ImagePage from "./ImagePage";
 import GeneratorPage from "./GeneratorPage";
 import UserProfilePage from "./UserProfilePage";
+import PageUnathorized from "./PageUnathorized";
 //import PageNotFound from "./PageNotFound";
 
-export { HomePage, ImagePage, GeneratorPage, UserProfilePage };
+export { HomePage, ImagePage, GeneratorPage, UserProfilePage, PageUnathorized };
